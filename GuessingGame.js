c06e6896f67b0be936c5f27f9da56adb8da8de93
Chan Nirak Choun <nirakchoun@gmail.com>
2024-06-27 09:44:42 -0500

    Update GuessingGame.js

--- GuessingGame.js
+++ GuessingGame.js
@@ -1,9 +1,14 @@
-console.log("Welcome to the Guessing Number game, where users guess a random number between 1 and 10.");
+alert("Welcome to the Guessing Number game, where users guess a random number between 1 and 10.");
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 let userGuess = prompt("Enter your guess: ");
 
 while (userGuess != randomNumber) {
+    if (user > randomNumber) {
+        console.log("Too high, try again");
+    } else {
+        console.log("Too low, try again");
+    }
     userGuess = Number(prompt("Enter your guess: "));
 }
 
-console.log(`You guess the right number, which is ${randomNumber}.`);
+alert(`You guess the right number, which is ${randomNumber}.`);
